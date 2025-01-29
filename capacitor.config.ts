b99dc4c2985c1a3150b34c8ed2e2d6c7dcd96bdc
Krishna vedantam <tenalikrishna25@gmail.com>
2025-01-29 13:22:34 +0530

    resolve build issue

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.claimit.app',
-  appName: 'ionic-claim-it',
-  webDir: 'www'
-};
-
-export default config;
