f86f95020e9a061b2a116e88b1c346cb89659520
Krishna vedantam <tenalikrishna25@gmail.com>
2025-01-29 22:13:00 +0530

    changes related to add image api

--- src/app/pages/admin/additem/additem.page.ts
+++ src/app/pages/admin/additem/additem.page.ts
@@ -140,7 +140,7 @@ export class AdditemPage implements OnInit {
   }
   addItem() {
     this.isModalOpen = true;
-    const url = 'http://100.28.242.219:8081/api/admin/listOfOrganisation';
+    const url = 'https://100.28.242.219.nip.io/api/admin/listOfOrganisation';
     this.http.get<any>(url).subscribe((response) => {
       this.addItemData = response;
       console.log(this.addItemData); 
@@ -174,7 +174,7 @@ export class AdditemPage implements OnInit {
       
       this.formData.append('image', this.files[0].file);
       this.formData.append('orgId', this.selectedOrgId);
-      this.http.post(' http://100.28.242.219/api/admin/image',  this.formData).subscribe(
+      this.http.post('https://100.28.242.219.nip.io/api/admin/image',  this.formData).subscribe(
         (response) => {
           this.formatResponse(response);          
         this.imageDataResponse = response
@@ -199,7 +199,7 @@ export class AdditemPage implements OnInit {
     this.formData.append('image', this.files[0].file);
     this.formData.append('orgId', this.selectedOrgId);
     this.formData.append('editedLabels', this.editableDescription)
-    this.http.post('http://100.28.242.219/api/admin/upload',  this.formData)
+    this.http.post('https://100.28.242.219.nip.io/api/admin/upload',  this.formData)
       .subscribe(response => {
         console.log('Data submitted:', response);
         this.isEditingDescription = false;
